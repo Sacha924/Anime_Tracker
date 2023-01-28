9c281a7f0e0209217958249423de23c7b726109e
Sacha924 <sacha.simon@edu.devinci.fr>
2023-01-28 23:17:35 +0100

    can add Anime

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,14 +1,37 @@
 import "./../styles/header.css";
-import { useState, useEffect } from "react";
+import { useState, useRef } from "react";
+
+const URL = "http://localhost:3000/animes/";
 
 function Header() {
+  const nameRef = useRef(null);
+  const lastEpisodeSeenRef = useRef(null);
+  const coverURLRef = useRef(null);
+  const animeEpisodeLinkRef = useRef(null);
   const [formAddAnime, setFormAddAnime] = useState(false);
 
   const searchForAnimes = async () => {
     // UTILISATION DE USEREF ? TENTER D ABORD SANS VOIR SI JE PEUX ACCEDER A LA VALUE SANS
+    
   };
 
-  const addAnime = async () => {
+  const addAnime = async (e) => {
+    e.preventDefault();
+    console.log(e.target.value);
+    const newAnime = {
+      name: nameRef.current.value,
+      lastEpisodeView: lastEpisodeSeenRef.current.value,
+      coverUrl: coverURLRef.current.value,
+      animeLink: animeEpisodeLinkRef.current.value,
+    };
+    console.log(JSON.stringify(newAnime));
+    await fetch(URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newAnime),
+    });
     setFormAddAnime(false);
   };
 
@@ -34,25 +57,24 @@ function Header() {
           <form onSubmit={addAnime} style={{ marginTop: "60px" }}>
             <div className="formField">
               <label> Name :</label>
-              <input type="text" placeholder="One Piece" />
+              <input type="text" placeholder="One Piece" ref={nameRef} />
             </div>
             <div className="formField">
               <label> Last Episode Seen :</label>
-              <input type="number" placeholder="1" />
+              <input type="number" placeholder="1" ref={lastEpisodeSeenRef} />
             </div>
             <div className="formField">
               <label> Cover URL :</label>
-              <input type="text" />
+              <input type="text" ref={coverURLRef} />
             </div>
             <div className="formField">
               <label> Anime Episode Link :</label>
-              <input type="text" />
+              <input type="text" ref={animeEpisodeLinkRef} />
             </div>
             <input className="submit" type="submit" value="Add" />
           </form>
         </div>
       )}
-
     </div>
   );
 }
