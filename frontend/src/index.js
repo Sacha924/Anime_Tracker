5a3a86e06dc402e86671830a48256be5ec4cc0f5
Sacha924 <sacha.simon@edu.devinci.fr>
2023-02-05 13:00:20 +0100

    Routes system

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
+import Login from "./Components/Login";
 import AnimeFrames from "./Components/AnimeFrames";
 import Header from "./Components/Header";
 import AnimeFilter from "./Components/AnimeFilter";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AnimeFilter>
-      <Header />
-      <AnimeFrames />
-    </AnimeFilter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route
+          path="/app"
+          element={
+            <AnimeFilter>
+              <Header />
+              <AnimeFrames />
+            </AnimeFilter>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
