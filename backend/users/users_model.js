a38d759160873f2978f2b95c3c9d4604d382852f
Sacha924 <sacha.simon@edu.devinci.fr>
2023-02-26 16:40:02 +0100

    Adding secret and QRCodeURL in the userModel

--- backend/users/users_model.js
+++ backend/users/users_model.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true, unique: true },
   password: { type: String, required: true },
+  secret: { type: String },
+  QRcodeURL: { type: String },
 });
 
 const User = mongoose.model("users", userSchema);
